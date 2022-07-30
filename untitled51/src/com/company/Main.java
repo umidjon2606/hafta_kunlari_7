b126fe11363b307e9aef044d98e798a42da5789d
umidjon2606 <110013094+umidjon2606@users.noreply.github.com>
2022-07-30 02:06:25 -0400

    Add files via upload

---
+++ untitled51/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int son = 9;
+
+        if (son == 1) {
+            System.out.println("Dushanba");
+        } else if (son == 2) {
+            System.out.println("Seshanba");
+        } else if (son == 3) {
+            System.out.println("Chorshanba");
+        } else if (son == 4) {
+            System.out.println("Payshanba");
+        } else if (son == 5) {
+            System.out.println("Juma");
+        } else if (son == 6) {
+            System.out.println("Shanba");
+        } else if (son == 7) {
+            System.out.println("Yakshanba");
+        } else {
+            System.out.println("bu kun yo'q");
+        }
+
+    }
+}
